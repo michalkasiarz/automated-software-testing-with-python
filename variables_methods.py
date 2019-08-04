b97b212fbf0d32383fad5bbaba4bee77f027fff8
Michal <michal.kasiarz@post.com>
2019-08-04 08:47:52 +0200

    variables

---
+++ variables_methods.py
@@ -0,0 +1,29 @@
+a = 5
+b = 10
+my_variable = 56
+any_variable_name = 10
+
+string_variable = "hello"
+single_quotes = 'strings can look like that'
+
+print(my_variable)
+print(string_variable)
+
+var1 = 10
+var2 = 5
+
+num1 = 4
+num2 = 6
+
+def my_print_method():
+
+    print(num1 * num2)
+
+my_print_method()
+
+def my_multiply_method(number_one, number_two):
+    return number_one * number_two
+
+result = my_multiply_method(5, 3)
+
+print(result)
