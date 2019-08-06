fd79e3aec2ebfcf32267d7c06222237c2afffba6
Michal <michal.kasiarz@post.com>
2019-08-06 19:56:24 +0200

    passing functions

---
+++ passing_functions.py
@@ -0,0 +1,13 @@
+def methodception(another):
+     return another
+
+def add_two_numbers():
+    return 35 + 77
+
+my_list = [13, 55, 21, 6, 2, 7, 13, 12, 14, 11, 20, 53, 52, 100]
+
+print(my_list)
+
+print(list(filter(lambda x: x % 2 != 0, my_list)))
+
+
